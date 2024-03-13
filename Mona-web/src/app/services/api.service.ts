6a252e075294ee73930c656d02b62c89a444c13e
Fazliddin Khayrullaev <fazliddinkhayrullaev4@gmail.com>
2024-03-13 12:39:04 +0500

    getting token values to cookie sign-in.component.ts jwt.service.ts

--- Mona-web/src/app/services/api.service.ts
+++ Mona-web/src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {User} from "../models/user";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,7 @@ export class ApiService {
     return this.http.post(BASE_URL + AUTH + SIGN_UP, user);
   }
 
-  signIn(user: Partial<any>) {
+  signIn(user: Partial<any>): Observable<any> {
     return this.http.post(BASE_URL + AUTH + SIGN_IN, user);
   }
 }
