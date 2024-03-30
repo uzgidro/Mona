7d322eca69a7e8c097758943ee2672454338a3a3
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-03-30 08:14:20 +0300

    message-tem custom event binding

--- Mona-web/src/app/message/message-item/message-item.component.ts
+++ Mona-web/src/app/message/message-item/message-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MessageModel } from '../../models/message';
 import { UserModel } from '../../models/user';
 
@@ -11,5 +11,21 @@ export class MessageItemComponent {
  @Input() message?:MessageModel
  @Input() selectedChat?:UserModel
  @Input()  editingMessage?: MessageModel
+
+
+ @Output() editMessageEvent=new EventEmitter<MessageModel>()
+ @Output() deleteMessageForMyselfEvent=new EventEmitter<MessageModel>()
+ @Output() deleteMessageForEveryoneEvent=new EventEmitter<MessageModel>()
+
+
+ deleteMessageForMyself(){
+  this.deleteMessageForMyselfEvent.emit(this.message)
+ }
+ deleteMessageForEveryone(){
+  this.deleteMessageForEveryoneEvent.emit(this.message)
+ }
+ editMessage(){
+  this.editMessageEvent.emit(this.message)
+ }
 
 }
