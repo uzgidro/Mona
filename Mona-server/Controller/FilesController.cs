881fe9ca84308ee81ab3b44031d553b7c6b2679a
suxa56 <suxa5637@mail.ru>
2024-04-09 12:13:17 +0500

    Download file method refactor, file not exists check

--- Mona-server/Controller/FilesController.cs
+++ Mona-server/Controller/FilesController.cs
@@ -11,13 +11,13 @@ namespace Mona.Controller;
 public class FilesController(ApplicationContext context) : ControllerBase
 {
     [HttpGet("download/{id}")]
-    public async Task<IActionResult> downloadFile(string id)
+    public async Task<IActionResult> DownloadFile(string id)
     {
         var fileInfo = await context.Files
             .AsNoTracking()
             .Where(item => item.Id.Equals(id) && !item.IsDeleted)
             .FirstOrDefaultAsync();
-        if (fileInfo == null) return NotFound();
+        if (fileInfo == null || !System.IO.File.Exists(fileInfo.Path)) return NotFound();
         await using var fileStream = new FileStream(fileInfo.Path, FileMode.Open, FileAccess.Read);
 
         return File(fileStream, "application/octet-stream", fileInfo.Name);
