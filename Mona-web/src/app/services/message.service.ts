2045ec2bdd44ae3b79ff5c3753cd4c5b8de78393
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-04-24 14:03:29 +0300

    message service

---
+++ Mona-web/src/app/services/message.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { ApiService } from "./api.service";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService{
+  constructor(private apiService:ApiService){}
+
+
+
+
+
+
+  sendMessage(){
+
+  }
+
+
+
+
+
+
+
+
+}
